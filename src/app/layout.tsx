dce9bec5fb8f0603f19929f4dce8f99737b800d2
sena <ying2181118469@gmail.com>
2024-06-18 15:28:17 +0800

    delete file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,7 @@ export const metadata: Metadata = {
     icons: {
      icon: '/icon.png'
     }
+    
 };
 
 export default function RootLayout ({children}: Readonly<{ children: React.ReactNode }>) {
